b27bad4edcf10bd52418402431cd4e3abf514c9b
Fede Krenn <fedekrenn@gmail.com>
2023-09-14 17:30:50 -0300

    Se corrige comportamiento de navegar con logueo

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,11 +1,11 @@
 import { createContext, useEffect, useState } from 'react'
-import { onAuthStateChanged } from 'firebase/auth'
+import { onAuthStateChanged, getAuth } from 'firebase/auth'
 import { auth } from '../config/firebaseConfig'
 
 const AuthContext = createContext()
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(getAuth())
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
